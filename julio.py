af6e65de676ce694be34748608e93e0bd25d1fc7
JuliopangkeyV1 <98557355+JuliopangkeyV1@users.noreply.github.com>
2022-01-30 15:50:19 +0800

    Add files via upload

---
+++ julio.py
@@ -0,0 +1,502 @@
+import sys
+import requests,json
+import os
+import time
+import random
+from time import sleep
+
+os.system('clear')
+
+#mengetik otomatis
+def mengetik(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+mengetik('\033[0;36m[❗❗❗SEDANG MENDOWNLOAD DATA\033[0;36m')
+mengetik('\033[1;93m[MOHON BERSABAR YAH TERIMAKASAIH\033[1;93m 😊')
+
+load = '•'
+count = 0
+
+for t in range(101):
+    time.sleep(0.1)
+    print(f'\rLoading [{load}]', end='', flush=True)
+    count += 1
+    if count == 3:
+       count = 0
+       load += '•'
+os.system('clear')
+
+os.system('pkg update')
+
+os.system('clear')
+
+mengetik('\033[1;33m[:>>>>>>>>>>> 20%\033[1;33m')
+mengetik('\033[1;33m•••[:>>>>>>>>>>>>>>> 40%\033[1;33m')
+mengetik('\033[1;33m••••••[:>>>>>>>>>>>>>>>>>>> 60%\033[1;33m')
+mengetik('\033[1;33m••••••••[:>>>>>>>>>>>>>>>>>>>>>> 70%\033[1;33m')
+mengetik('\033[1;33m••••••••••[:>>>>>>>>>>>>>>>>>>>>>>>>>> 80%\033[1;33m')
+mengetik('\033[1;33m••••••••••••[:>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 100%\033[1;33m')
+
+os.system('pkg upgrade')
+
+os.system('clear')
+
+load = '•'
+count = 0
+
+for t in range(101):
+    time.sleep(0.1)
+    print(f'\rLoading [{load}]', end='', flush=True)
+    count += 1
+    if count == 3:
+       count = 0
+       load += '•'
+
+os.system('pip install mechanize')
+
+mengetik('\033[1;33mSELESAI MENDOWNLOAD DATA ')
+
+mengetik('\033[1;33m BERHASIL MENDOWNLOAD DATA ❗❗❗ ')
+
+os.system('clear')
+
+mengetik('                     ``..--------.``             ')         
+mengetik('            .:/osydmNNNNMMMMMMMMMMMNNNmdhso+:.`     ')       
+mengetik('        -+hmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNho:`   ')     
+mengetik('      :hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm+   ')    
+mengetik('     +NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy   ')   
+mengetik('    -NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+  ')   
+mengetik('    hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN` ')   
+mengetik('   .MMMMMMMNmmdmmNMMMMMMMMMMMMMMMMMMMMMNmddddmNMMMMMMM/ ')   
+mengetik('   oMMMMms/:-..`..-/sdNMMMMMMMMMMMMNds/-.```.--:smMMMMh ')   
+mengetik('   dMMNsoyddo::--..` `-sNMMMMMMMMNs-`  `.---:omdyosmMMN')  
+mengetik('  `NMMhmMMMMNmdy/-.::`  .mMMMMMMm-  `-:.-/yhmNMMMMmhMMM.')   
+mengetik('  -MMMMMMMMMMMMMMms-.::..hMMMMMMd.-/:.-smMMMMMMMMMMMMMM-')   
+mengetik('  /MMMMMMMMMMMMMMMMNh-`:dMMMMMMMMm+.-yNMMMMMMMMMMMMMMMM/')  
+mengetik('  +MMMMMMNmdhyyhdmNMMN/ `hMMMMMMM::hMMMNmhyyyhdmNMMMMMM+')   
+mengetik('  oMMMMNs-`      `-/ymN- .NMMMMMMhMMNy/.       `-omMMMMo')   
+mengetik('  oMMh+.             .h:  yMMMMMMMMd.             -oydNo')   
+mengetik('  oMMs``.-::///+++ooshM-  oMMMMMMMMMdsssyyyyyyyyhhdmdhh/')   
+mengetik('  oMMdmNMMMMMMMMMMMMMMM-  oMMMMMMMMMMMMMMMMMMMMMMMMMMMMo')   
+mengetik('  -NMMMMMMMMMMMMMMMMMMN`  sMMMMMMMMMMMMMMMMMMMMMMMMMMMMo')   
+mengetik('   /MMMMMMMMMMMMMMMMMMh   sMMMMMMMMMMMMMMMMMMMMMMMMMMMM:')  
+mengetik('    /NMMMMMMMMMMMMMMMM:   yMMMMMMMMMMMMMMMMMMMMMMMMMMMs ')   
+mengetik('   s/.yMMMMMMMMMMMMMd/    yMMMMMMMdNMMMMMMMMMMMMMMMMy:  ')   
+mengetik('   .N/ ./oossoosyhm.  -   mMMMMMMMhooMMMNhddmmddhy+.s+  ')   
+mengetik('    +N:`o` yNNNNMMh  sm` -MMMMMMMMMMyhMMMmhhyo` ./ :N.  ')   
+mengetik('     sN::y``oNMMMMMh+Mm  +MMMMMMMMMMMMMMMMMMm- -m/-No   ')   
+mengetik('     `yN+/y` .+hmMMMMMMs +MMMMMmhMMMMMMMMMmo` /Ns.md` ')     
+mengetik('      `yMy-o/`  `-+shhs- `-/++:.`:hNNNmhs:` `od/-mm.    ')   
+mengetik('       `yMd-:so:`         `/h/`    ...`   ./ho.:md.   ')     
+mengetik('        `sMm:`:hmho/-.````ymNNh-     `.:ohmNs`+Nd.  ')       
+mengetik('          oNN+ `:hNMNNmmmhhyyysssyhddmNMMMMd:yMy`       ')   
+mengetik('           /NMo  `ohdmmNMMMMMMMMMMMMMMMMMMh/dNo`      ')     
+mengetik('            :mMo `NMNmdhyo+++++omMMMMMMMMyoNN/      ')       
+mengetik('             .hMo`mMMMMMMm.    /NMMMMMMNsyMd-     ')         
+mengetik('              `sN+sMMMMMMM:    mMMMMMMMddMy`    ')           
+mengetik('                /mmMMMMMMs     oMMMMMMMMN+    ')             
+mengetik('                 .yMMMMMM.  .` .MMMMMMMm:   ')               
+mengetik('                     -dMMMd     yMMMMd:  ')           
+mengetik('                      /dNM-   .NMNy: ')              
+mengetik('                        `-`   `:- ')
+print ('')
+
+load = '•'
+count = 0
+
+for t in range(101):
+    time.sleep(0.1)
+    print(f'\rLoading [{load}]', end='', flush=True)
+    count += 1
+    if count == 3:
+       count = 0
+       load += '•'
+
+print ('')
+mengetik('\033[1;32m[CAYBER--JULIO--PANGKEY]\033[1;32m')
+print ('')
+
+print ('\033[1;96m+\033[93m===================================================\033[1;96m ')
+print ('\033[1;90m [\033[1;93m+\033[1;90m]\033[1;95m Author   \033[1;90m: \033[1;96m JULIO.PANGKEY ')
+print ('\033[1;90m [\033[1;93m+\033[1;90m]\033[1;95m youtube  \033[1;90m:\033[1;96m Kz.tutorial ')
+print ('\033[1;90m [\033[1;93m+\033[1;90m]\033[1;95m facebook \033[1;90m:\033[1;96m : @Juliopangkey ')
+print ('\033[1;90m [\033[1;93m+\033[1;90m]\033[1;95m  whatsaap \033[1;90m:\033[1;96m : 085757051840 ')
+print ('\033[1;96m [\033[93m==================================================\033[1;96m ')
+mengetik('\033[1;32mHAI SLAMAT DATANG DI TOOLS KAMI ')
+mengetik('SEBELUM LANJUT DI SUBSCRIBE DULUH YT KZ.TUTORIAL')
+mengetik('TERIMAKASIH YANG SUDAH SUBSCRIBE ❗❗❗')
+
+load = '•'
+count = 0
+
+for t in range(101):
+    time.sleep(0.1)
+    print(f'\rLoading [{load}]', end='', flush=True)
+    count += 1
+    if count == 3:
+       count = 0
+       load += '•'
+
+print ('BERHASIL LOGIN')
+
+os.system('clear')
+
+print ('\033[1;96m@[=================================]@\033[1;36m')
+print ('@')
+mengetik('\033[1;95m@    [SPAM CALL CAYBER]           @\033[1;95m')
+print ('@')
+mengetik('\033[1;93m@    [UPDATE SISTEM 2022]         @\033[1;93m')
+print ('@')
+mengetik('\033[1;31m@    [MAHON DI GUNAKA BUAT BAIK]  @ \033[1;31m')
+print ('@')
+print ('\033[1;96m@[==================================@\033[96m')
+print ('')
+
+load = '•'
+count = 0
+
+for t in range(101):
+    time.sleep(0.1)
+    print(f'\rLoading [{load}]', end='', flush=True)
+    count += 1
+    if count == 3:
+       count = 0
+       load += '•'
+print ('PROSES PENGINSTALAN SC ')
+mengetik('\033[1;95m[MOHON BERSABAR PENGINSTALAN SC] \033[1;95m')
+
+load = '•'
+count = 0
+
+for t in range(101):
+    time.sleep(0.1)
+    print(f'\rLoading [{load}]', end='', flush=True)
+    count += 1
+    if count == 3:
+       count = 0
+       load += '•'
+
+os.system('clear')
+
+print ('')
+
+os.system('pkg update && pkg upgrade') 
+os.system('pkg install git')
+os.system('pkg install python3')
+
+# tampilan
+os.system('clear')
+
+print ('\033[1;96m+\033[93m===========================================>')
+print ('\033[1;90m [\033[1;93m+\033[1;90m]\033[1;95m   Author : Juliopangkey\033[1;90m')
+print ('\033[1;90m [\033[1;93m+\033[1;90m]\033[1;95m  youtube : KZ.tutorial \033[1;90m')
+print ('\033[1;90m [\033[1;93m+\033[1;90m]\033[1;95m  facebook: @Juliopangkey \033[1;90m')
+print ('\033[1;90m [\033[1;93m+\033[1;90m]\033[1;95m  whatsaap: 085757051840 \0336[1;90m')
+print ('\033[1;96m [\033[93m==========================================>')
+mengetik('NOMOR DI AWALI DENGGAN 08××××××××')
+mengetik('===============================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor}).text
+if 'terkirim' in req:
+     mengetik('SPAM BERHASIL TERKIRIM')
+else:
+     mengetik('SPAM GAGAL COBA LAGI')
+mengetik('===============================')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+
+mengetik('================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('=================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+
+mengetik('==================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('==================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('===================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('====================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('====================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('====================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('=====================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('=====================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('======================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+
+mengetik('======================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('=======================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('========================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('========================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('========================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('========================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('========================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+mengetik('========================================')
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
+
+# Run nomor
+nomor = input('[+] Nomor target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor})
+if 'terkirim' in req:
+     print ('SPAM BERHASIL TERKIRIM')
+else:
+     print ('SPAM GAGAL TERKIRIM')
